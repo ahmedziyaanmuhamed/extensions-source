1e22594e98982952e7cd61cb97b9891255afbbb5
ahmedziyaanmuhamed <86360363+ahmedziyaanmuhamed@users.noreply.github.com>
2025-07-08 21:54:22 +0500

    Update AnimatedGlitchedComics.kt
    update ulr and change theme to Iken

--- src/en/animatedglitchedcomics/src/eu/kanade/tachiyomi/extension/en/animatedglitchedcomics/AnimatedGlitchedComics.kt
+++ src/en/animatedglitchedcomics/src/eu/kanade/tachiyomi/extension/en/animatedglitchedcomics/AnimatedGlitchedComics.kt
@@ -1,10 +1,10 @@
 package eu.kanade.tachiyomi.extension.en.animatedglitchedcomics
 
-import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+import eu.kanade.tachiyomi.multisrc.iken.Iken
 
 class AnimatedGlitchedComics : Keyoapp(
     "Animated Glitched Comics",
-    "https://agrcomics.com",
+    "https://agrcomics.org",
     "en",
 ) {
     override val versionId = 2
